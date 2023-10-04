8760ede2231c827e1898fd5c68a9390d57e8351d
chayapatr <chayapatr.arc@gmail.com>
2023-10-04 23:24:51 +0700

    feat: circle setting

--- src/components/Joy.svelte
+++ src/components/Joy.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte'
-	import { slide, fly } from 'svelte/transition'
+	import { slide } from 'svelte/transition'
+    import { pinch, rotate } from 'svelte-gestures';
 
 	export let x = 0
 	export let y = 0
@@ -9,11 +10,44 @@
 	let drag = false
 	let dp = {}
 
+    let data
+    let scale = 1
+    let rotation = 0
+
+    let prevScale = 1
+    let prevRotation = 0
+
+    let previousTouch
+
+    const pinchHandler = (e) => {
+        scale = e.detail.scale;
+        // x = e.detail.center.x;
+        // y = e.detail.center.y;
+    }
+
+    const rotateHandler = (e) => {
+        rotation = e.detail.rotation;
+        // prevRotation = rotation
+        // x = e.detail.center.x;
+        // y = e.detail.center.y;
+    }
+
 	const g = (e) => {
-		if (drag) {
-			x += e.movementX
-			y += e.movementY
-		}
+        const touch = e.touches[0];
+
+        if (previousTouch) {
+            // be aware that these only store the movement of the first touch in the touches array
+            let changeX = touch.pageX - previousTouch.pageX;
+            let changeY = touch.pageY - previousTouch.pageY;
+
+            if (drag) {
+			    x += changeX
+			    y += changeY
+                data = changeX
+		    }
+        };
+
+        previousTouch = touch;
 	}
 
 	const lineDraw = (ax, ay, bx, by) => {
@@ -61,44 +95,40 @@
 			h: document.documentElement.clientHeight,
 			sh: document.documentElement.scrollHeight
 		}
-		const elp = {
-			w: el.offsetWidth,
-			h: el.offsetHeight
-		}
-		const sp = {
-			w: (dp.w - c.maxw) / 2 - elp.w,
-			h: dp.h - elp.h
-		}
 
 		x = pos.x
 		y = pos.y
-
-		lineDraw(10, 300, 20, 600)
 	})
 </script>
 
-<svelte:window on:mousemove={g} on:mouseup={() => (drag = false)} />
+<svelte:window on:touchmove={g} on:touchend={() => {drag = false; previousTouch = undefined}} />
 
-<div>
+<div style={`background-color: rgba(${x / dp.w * 255}, ${y / dp.h * 255}, ${((scale - 1) % 3) * 255}, ${(360 - rotation) / 360})`} class="w-[100vw] h-[100vh]">
 	<div class="absolute bottom-0 left-0 p-4">
-		<div>screen: {dp.w} {dp?.h}</div>
+		<div>screen: {dp.w} {dp.h} {prevRotation}</div>
+		<!-- <div>data: {scale} {rotation}</div> -->
 	</div>
 	<div
-		class={`absolute hover:cursor-grab select-none px-4 md:p-0`}
+		class={`relative hover:cursor-grab select-none`}
 		bind:this={el}
-		on:mousedown={() => (drag = true)}
-		style={`left: ${x}px; top: ${y}px; z-index:1;`}
+		on:touchstart={() => (drag = true)}
+		style={`left: ${x}px; top: ${y}px; z-index:1;transform:scale(${scale}) rotate(${rotation}deg)`}
 		transition:slide
 	>
-		<div class={`border rounded-full w-32 aspect-square flex justify-center items-center`}>
+		<div
+            class={`border w-32 aspect-square flex justify-center items-center bg-neutral-50/60 text-neutral-900 rounded-full text-center`}
+            use:pinch
+            on:pinch="{pinchHandler}"
+            use:rotate
+            on:rotate="{rotateHandler}"
+        >
 			<div class="flex flex-col justify-center items-center text-xs">
 				<div>drag: {drag}</div>
-				<div>abs: {x} {y}</div>
-				<div>rel: {x} {y}</div>
+				<div>{Math.floor(x / dp.w * 255)} {Math.floor(y / dp.h * 255)} {Math.floor(((scale - 1) % 3) * 255)} {Math.floor((360 - rotation) / 360 * 1000) / 1000}</div>
 			</div>
 		</div>
 		<div class="h-0 md:h-12"></div>
 	</div>
-    <div class="w-20 aspect-square " style={`background-color: rgb(${x % 255} ${y % 255} 100)`} />
+    <!-- <div class="w-20 aspect-square " style={`background-color: rgb(${x % 255} ${y % 255} 100)`} /> -->
 	<div id="line" />
 </div>
